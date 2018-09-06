84f31936f412cd97ed0189b3bc579008d21e2c68
Mariusz Krotos <mar.krotos@gmail.com>
2018-09-06 12:00:43 +0200

    Repository init

---
+++ src/main/java/com/in28minutes/springboot/rest/example/springboot2restservicebasic/students/StudentRepository.java
@@ -0,0 +1,7 @@
+package com.in28minutes.springboot.rest.example.springboot2restservicebasic.students;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student,Long>{}
